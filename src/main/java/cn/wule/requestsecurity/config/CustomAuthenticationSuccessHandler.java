8c75abbcfda8a23ba5d178c980bc1665927c49c6
吴乐 <wl1030ds@gmail.com>
2023-11-18 10:45:53 +0800

    ADD

--- src/main/java/cn/wule/requestsecurity/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/cn/wule/requestsecurity/config/CustomAuthenticationSuccessHandler.java
@@ -11,13 +11,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 
 /**
