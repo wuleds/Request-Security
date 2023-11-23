93b4f1ceebff67194c0249dec535c186109d64e2
吴乐 <wl1030ds@gmail.com>
2023-11-23 23:37:37 +0800

    ADD

--- src/main/java/cn/wule/requestsecurity/config/AppAuthenticationFailHandler.java
+++ src/main/java/cn/wule/requestsecurity/config/AppAuthenticationFailHandler.java
@@ -4,15 +4,12 @@ package cn.wule.requestsecurity.config;
 import cn.wule.requestsecurity.vo.HttpRequest;
 import com.google.gson.Gson;
 import jakarta.annotation.Resource;
-import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
